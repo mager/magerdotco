055e0956130fb6b5c5690911820aadf84bfa7d9c
Mager <andrew.mager@gmail.com>
2023-01-14 15:19:59 -0600

    Fix bad blog links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import partytown from "@astrojs/partytown";
+import vercel from '@astrojs/vercel/serverless';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mager.co',
+	output: 'server',
 	integrations: [
 		mdx(),
 		sitemap(),
