48be366ec4d55ebf2a2790a8c134876e53278109
Mager <andrew.mager@gmail.com>
2023-04-08 09:16:50 -0500

    Fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import partytown from "@astrojs/partytown";
 import vercel from '@astrojs/vercel/serverless';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mager.co',
 	output: 'server',
+	adapter: vercel(),
 	integrations: [
 		mdx(),
 		sitemap(),
